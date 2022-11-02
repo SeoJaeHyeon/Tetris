f7a1eeaa11d4cbc5eb603b938ad64e1607660d5a
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-02 23:15:05 +0900

    체크리스트 설정

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 11월 1주차 CheckList
+        /*
+        재현 : 게임 초기 화면 및 게임모드 구성
+        인범 : 게임 환경 설정 화면
+        세현 : 게임 실행화면 구성
+         */
+
     }
 }
