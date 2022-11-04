cc7b5e0917803f048ea916a86f9f7a213ff824a9
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-04 15:17:49 +0900

    게임 모드 선택 화면 구성(수정3)

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -2,18 +2,22 @@ package com.example.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tetris.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // 11월 1주차 CheckList
-        /*
-        재현 : 게임 초기 화면 및 게임모드 구성
-        인범 : 게임 환경 설정 화면
-        세현 : 게임 실행화면 구성
-         */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
 
     }
 }
