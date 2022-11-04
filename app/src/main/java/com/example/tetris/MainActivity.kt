5915bbc0db846214fbe77d6530eaf03702578522
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-04 15:36:27 +0900

    1. 인범, 세현이가 만들 레이아웃 파일 만들어놓음.(그 파일 안에 레이아웃 만드세요) 2. 게임 시작에서 게임모드 넘어가고 게임 모드에서 각각의 레이아웃으로 넘어가게 해놓음 3. 게임 시작에서 환경설정으로 넘어가게 해 놓음

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.tetris.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    fun choiceGamemode() {
+
+        val btn_classic: TextView = findViewById(R.id.btn_classicmode)
+        val btn_hard: TextView = findViewById(R.id.btn_hardmode)
+        val btn_timeAttack: TextView = findViewById(R.id.btn_timeattackmode)
+
+        btn_classic.setOnClickListener {
+            setContentView(R.layout.activity_classicmode)
+        }
+
+        btn_hard.setOnClickListener {
+            setContentView(R.layout.activity_hardmode)
+        }
+
+        btn_timeAttack.setOnClickListener {
+            setContentView(R.layout.activity_timeattackmode)
+        }
+
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +39,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnGamestart.setOnClickListener {
             setContentView(R.layout.activity_gamemode)
+            choiceGamemode()
+        }
+
+        binding.btnSetting.setOnClickListener {
+            setContentView(R.layout.activity_setting)
         }
 
 
