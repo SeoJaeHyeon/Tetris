5cd479787b5beed3cbd899310698e35b908ad3bd
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-09 02:09:03 +0900

    classicMode에서 게임 화면 그리드 모양으로 출력 (일단 기본 버전) -> 추후 수정해야함

--- app/src/main/java/com/example/tetris/ClassicModeActivity.kt
+++ app/src/main/java/com/example/tetris/ClassicModeActivity.kt
@@ -3,6 +3,8 @@ package com.example.tetris
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tetris.databinding.ActivityClassicmodeBinding
 
@@ -11,12 +13,23 @@ import com.example.tetris.databinding.ActivityClassicmodeBinding
 class ClassicModeActivity : AppCompatActivity() {
     lateinit var binding: ActivityClassicmodeBinding
 
+    val ROW = 29
+    val COL = 16
+    var boardView = Array(ROW) {
+        arrayOfNulls<ImageView>(COL)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityClassicmodeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.gridmain.rowCount = ROW
+        binding.gridmain.columnCount = COL
+
+        initRender(boardView, binding.gridmain, ROW, COL)
         //버튼 눌렸을 때
         binding.imgLeft.setOnClickListener {
             left()
@@ -32,6 +45,17 @@ class ClassicModeActivity : AppCompatActivity() {
         }
         binding.imgStop.setOnClickListener {
             startActivity(Intent(this, StopActivity::class.java)) // 그만하기 버튼 누르면 StopActivity로 이동 -재현이가 해놓음
+        }
+    }
+
+    fun initRender(arr: Array<Array<ImageView?>>, grid: androidx.gridlayout.widget.GridLayout, row: Int, col: Int) {
+        val inflater = LayoutInflater.from(this)
+        for (i in 0 until row) {
+            for (j in 0 until col) {
+                arr!![i][j] =
+                    inflater.inflate(R.layout.inflate_image, grid, false) as ImageView
+                grid.addView(arr[i][j])
+            }
         }
     }
 
