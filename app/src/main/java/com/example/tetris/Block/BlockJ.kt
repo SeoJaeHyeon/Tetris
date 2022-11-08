5056aa4957edf61edb6f76cff33bc22c78865409
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-08 20:11:06 +0900

    1. 블럭 생성 2. 블럭 회전(rotation은 생략)

---
+++ app/src/main/java/com/example/tetris/Block/BlockJ.kt
@@ -0,0 +1,36 @@
+package com.example.tetris.Block
+
+class BlockJ(var row: Int, var col: Int): Block(row, col) {
+
+    init {
+        number = 4
+        point2 = Point(row + 1, col )
+        point3 = Point(row + 2, col )
+        point4 = Point(row + 2, col - 1)
+    }
+
+    override fun blockDown() {
+        point1.down()
+        point2.down()
+        point3.down()
+        point4.down()
+    }
+
+    override fun blockLeft() {
+        point1.left()
+        point2.left()
+        point3.left()
+        point4.left()
+    }
+
+    override fun blockRight() {
+        point1.right()
+        point2.right()
+        point3.right()
+        point4.right()
+    }
+
+    override fun rotation() {
+
+    }
+}
