5c5cb631a05b5edea8065e662eaf9dc7a970db8b
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-09 19:10:30 +0900

    1. gameFrame 선 투명하게 변경 2. 게임시작하면 블럭 하나 등장 3. 각각의 움직임 버튼 누르면 해당 동작 수행 및 게임 프레임에 출력 4. ClassicModeActivity에 출력될 블럭을 담을 변수 선언 5. 각 코드에 주석

--- app/src/main/java/com/example/tetris/ClassicModeActivity.kt
+++ app/src/main/java/com/example/tetris/ClassicModeActivity.kt
@@ -17,12 +17,15 @@ import kotlin.random.Random
 class ClassicModeActivity : AppCompatActivity() {
     lateinit var binding: ActivityClassicmodeBinding
 
-    val ROW = 20
-    val COL = 10
-    var gameFrame = Array(ROW) {
+    val ROW = 20 // gameFrame의 row
+    val COL = 10 // gameFrame의 col
+    var gameFrame = Array(ROW) { // gameFrame의 이차원 배열
+        // 각 행들의 배열의 타입은 ImageView ! 나중에 초기화하기 위해 arrayOfNulls 배열 사용
         arrayOfNulls<ImageView>(COL)
     }
+    // 블럭들을 랜덤하게 나오기 위해 블럭의 번호를 난수로 저장
     var randomNum: Int = Random.nextInt(0, 7)
+    // 랜덤하게 얻은 블럭을 Block에 저장(게임화면에서 움직일 블럭)
     var block: Block = randomBlockChoice(randomNum, 1, COL / 2)
 
 
@@ -33,44 +36,41 @@ class ClassicModeActivity : AppCompatActivity() {
         binding = ActivityClassicmodeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.gridmain.rowCount = ROW
-        binding.gridmain.columnCount = COL
+        binding.gridmain.rowCount = ROW // gridLayout의 row
+        binding.gridmain.columnCount = COL // gridLayout의 col
 
+        // 게임 시작 후 gridLayout에 게임화면 생성
         gameFrameSetting(gameFrame, binding.gridmain, ROW, COL)
-        printBlock()
-
+        printBlock() // 움직일 블럭 생성
 
         //버튼 눌렸을 때
         binding.imgLeft.setOnClickListener {
-            removeBlock()
-            block.blockLeft()
-            printBlock()
+            removeBlock() // 블럭을 원래 gridLayout의 배경으로 다시 변경
+            block.blockLeft() // 블럭을 왼쪽으로 움직임
+            printBlock() // 움직인 블럭을 다시 그림
         }
         binding.imgRight.setOnClickListener {
-            removeBlock()
-            block.blockRight()
-            printBlock()
+            removeBlock() // 블럭을 원래 gridLayout의 배경으로 다시 변경
+            block.blockRight() // 블럭을 오른쪽으로 움직임
+            printBlock() // 움직인 블럭을 다시 그림
         }
         binding.imgDown.setOnClickListener {
-            removeBlock()
-            block.blockDown()
-            printBlock()
+            removeBlock() // 블럭을 원래 gridLayout의 배경으로 다시 변경
+            block.blockDown() // 블럭을 아래로 움직임
+            printBlock() // 움직인 블럭을 다시 그림
         }
         binding.imgChange.setOnClickListener {
-            removeBlock()
-            block.rotation()
-            printBlock()
+            removeBlock() // 블럭을 원래 gridLayout의 배경으로 다시 변경
+            block.rotation() // 블럭을 회전시킴
+            printBlock() // 움직인 블럭을 다시 그림
         }
         binding.imgStop.setOnClickListener {
-            startActivity(
-                Intent(
-                    this,
-                    StopActivity::class.java
-                )
-            ) // 그만하기 버튼 누르면 StopActivity로 이동
+            // 그만하기 버튼 누르면 StopActivity로 이동
+            startActivity(Intent(this, StopActivity::class.java))
         }
     }
 
+    // 다음에 나올 블럭 랜덤하게 지정(블럭의 number를 통해 지정)
     fun randomBlockChoice(number: Int, row: Int, col: Int): Block {
         val randomBlock: Block
         when(number) {
@@ -85,19 +85,24 @@ class ClassicModeActivity : AppCompatActivity() {
         return randomBlock
     }
 
-
+    // 게임 화면 설정 -> 게임화면에 사용될 이차원배열, 이차원배열을 그려줄 gridLayout, row, col을 매개변수로 받음
     fun gameFrameSetting(arr: Array<Array<ImageView?>>, grid: androidx.gridlayout.widget.GridLayout, row: Int, col: Int) {
+        // 해당 context(환경에 대한 전역정보) LayoutInflater를 가져옴(xml을 View객체로 변환)
         val inflater = LayoutInflater.from(this)
         for (i in 0 until row) {
             for (j in 0 until col) {
-                arr!![i][j] = inflater.inflate(R.layout.inflate_image, grid, false) as ImageView
+                // 게임화면에서 사용되는 이차원 배열의 각 원소에 gameframe_image를 넣음
+                arr!![i][j] = inflater.inflate(R.layout.gameframe_image, grid, false) as ImageView
+                // 위의 배열을 gridLayout에 맞추어 이미지 삽입
                 grid.addView(arr[i][j])
             }
         }
     }
+    // 블럭의 색 결정하는 함수
     fun blockColor(number: Int): Int {
         val color: Int
         when(number) {
+            // 블럭의 number에 따라 각각 그려질 블럭 색깔 지정
             0 -> color = R.drawable.skyblueblockl
             1 -> color = R.drawable.yellowblocko
             2 -> color = R.drawable.redblockz
@@ -109,14 +114,16 @@ class ClassicModeActivity : AppCompatActivity() {
         return color
     }
 
-
+    // 블럭을 gameFrame에 보여주는 함수
     fun printBlock() {
+        // 블럭이 지정하는 인덱스에 맞추어 블럭 출력 -> gameFrame의 배열 블럭은 항상 이미지가 있어 null이 될 수 없음
         gameFrame[block.point1.x][block.point1.y]!!.setImageResource(blockColor(block.number))
         gameFrame[block.point2.x][block.point2.y]!!.setImageResource(blockColor(block.number))
         gameFrame[block.point3.x][block.point3.y]!!.setImageResource(blockColor(block.number))
         gameFrame[block.point4.x][block.point4.y]!!.setImageResource(blockColor(block.number))
     }
 
+    // gameFrame에서 블럭을 다시 원래 배경으로 바꾸는 함수(이동할 때 사용)
     fun removeBlock() {
         gameFrame[block.point1.x][block.point1.y]!!.setImageResource(R.drawable.gameframe)
         gameFrame[block.point2.x][block.point2.y]!!.setImageResource(R.drawable.gameframe)
@@ -124,7 +131,7 @@ class ClassicModeActivity : AppCompatActivity() {
         gameFrame[block.point4.x][block.point4.y]!!.setImageResource(R.drawable.gameframe)
     }
 
-
+    // 블럭이 내려오는 걸 지연하는 함수인데 했는데 적용이 안되서 다시 해야할 듯
     fun runDelay(second: Long) {
         runBlocking {
             launch {
