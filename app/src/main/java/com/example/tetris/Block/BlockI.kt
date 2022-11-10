30105b1bac53c4c5bcad7214f1d37b864ee6a9a9
DESKTOP-AMVM8NF\jaehy <jaehyeon4037@gmail.com>
2022-11-10 22:36:28 +0900

    (블럭 움직이는 조건 = 벽에 부딪혔는지 확인 -> 다른 블럭과 부딪혔는지 확인) T 자 블럭 빼고 나머지 블럭들은 왼쪽 벽 or 왼쪽 다른 블럭, 오른쪽 벽 or 오른쪽 다른 블럭, 바닥 or 밑의 다른 블럭 만나면 더이상 움직이지 않음

--- app/src/main/java/com/example/tetris/Block/BlockI.kt
+++ app/src/main/java/com/example/tetris/Block/BlockI.kt
@@ -73,37 +73,28 @@ class BlockI(var row: Int, var col: Int): Block(row, col) {
 
     fun touchBottomBlock(arr: CompareArray): Boolean {
         if(!isRotation) {
-            if(arr.arr[point4.x + 1][point4.y] == 1) return true
-            else return false
+            return arr.arr[point4.x + 1][point4.y] == 1
         } else {
-            if(arr.arr[point1.x + 1][point1.y] == 1 || arr.arr[point2.x + 1][point2.y] == 1 ||
-                arr.arr[point3.x + 1][point3.y] == 1 || arr.arr[point4.x + 1][point4.y] == 1 ) {
-                return true
-            } else return false
+            return arr.arr[point1.x + 1][point1.y] == 1 || arr.arr[point2.x + 1][point2.y] == 1 ||
+                    arr.arr[point3.x + 1][point3.y] == 1 || arr.arr[point4.x + 1][point4.y] == 1
         }
     }
 
     fun touchLeftBlock(arr: CompareArray): Boolean {
         if(!isRotation) {
-            if( arr.arr[point1.x][point1.y - 1] == 1 || arr.arr[point2.x][point2.y - 1] == 1 ||
-                arr.arr[point3.x][point3.y - 1] == 1 || arr.arr[point4.x][point4.y - 1] == 1 ) {
-                return true
-            } else return false
+            return arr.arr[point1.x][point1.y - 1] == 1 || arr.arr[point2.x][point2.y - 1] == 1 ||
+                    arr.arr[point3.x][point3.y - 1] == 1 || arr.arr[point4.x][point4.y - 1] == 1
         } else {
-            if(arr.arr[point4.x][point4.y - 1] == 1) return true
-            else return false
+            return arr.arr[point4.x][point4.y - 1] == 1
         }
     }
 
     fun touchRightBlock(arr: CompareArray): Boolean {
         if(!isRotation) {
-            if(arr.arr[point1.x][point1.y + 1] == 1 || arr.arr[point2.x][point2.y + 1] == 1 ||
-                arr.arr[point3.x][point3.y + 1] == 1 || arr.arr[point4.x][point4.y + 1] == 1) {
-                return true
-            } else return false
+            return arr.arr[point1.x][point1.y + 1] == 1 || arr.arr[point2.x][point2.y + 1] == 1 ||
+                    arr.arr[point3.x][point3.y + 1] == 1 || arr.arr[point4.x][point4.y + 1] == 1
         } else {
-            if(arr.arr[point2.x][point2.y + 1] == 1) return true
-            else return false
+            return arr.arr[point2.x][point2.y + 1] == 1
         }
     }
 
--- app/src/main/java/com/example/tetris/Block/BlockJ.kt
+++ app/src/main/java/com/example/tetris/Block/BlockJ.kt
@@ -13,27 +13,33 @@ class BlockJ(var row: Int, var col: Int): Block(row, col) {
 
     override fun blockDown(arr: CompareArray) {
         if(!(arr.touchFloor(this))) { // 블럭들이 바닥에 안닿았으면 이동가능
-            point1.down()
-            point2.down()
-            point3.down()
-            point4.down()
+           if(!touchBottomBlock(arr)) { // 다른블럭이랑 밑 부분이 안부딪히면 이동 가능
+               point1.down()
+               point2.down()
+               point3.down()
+               point4.down()
+           }
         }
     }
 
     override fun blockLeft(arr: CompareArray) {
         if(!(arr.touchLeft(this))) { // 블럭들이 왼쪽 벽에 닿지 않았으면 이동가능
-            point1.left()
-            point2.left()
-            point3.left()
-            point4.left()
+            if(!touchLeftBlock(arr)) {// 다른블럭이랑 왼쪽 부분이 안부딪히면 이동 가능
+                point1.left()
+                point2.left()
+                point3.left()
+                point4.left()
+            }
         }
     }
     override fun blockRight(arr: CompareArray) {
         if(!(arr.touchRight(this))) { // 블럭들이 오른쪽 벽에 닿지 않았으면 이동가능
-            point1.right()
-            point2.right()
-            point3.right()
-            point4.right()
+            if(!touchRightBlock(arr)) {// 다른블럭이랑 오른쪽 부분이 안부딪히면 이동 가능
+                point1.right()
+                point2.right()
+                point3.right()
+                point4.right()
+            }
         }
     }
     override fun rotation() {
@@ -89,5 +95,48 @@ class BlockJ(var row: Int, var col: Int): Block(row, col) {
         }
 
     }
+
+    fun touchBottomBlock(arr: CompareArray): Boolean {
+        if(!isRotation1 && !isRotation2 && !isRotation3) {
+            return arr.arr[point3.x + 1][point3.y] == 1 || arr.arr[point4.x + 1][point4.y] == 1
+        } else if(isRotation1 && !isRotation2 && !isRotation3){
+            return arr.arr[point1.x + 1][point1.y] == 1 || arr.arr[point2.x + 1][point2.y] == 1 ||
+                    arr.arr[point3.x + 1][point3.y] == 1
+        } else if(isRotation1 && isRotation2 && !isRotation3) {
+            return arr.arr[point2.x + 1][point2.y] == 1 || arr.arr[point4.x + 1][point4.y] == 1
+        } else {
+            return arr.arr[point1.x + 1][point1.y] == 1 || arr.arr[point2.x + 1][point2.y] == 1 ||
+                    arr.arr[point4.x + 1][point4.y] == 1
+        }
+    }
+
+    fun touchLeftBlock(arr: CompareArray): Boolean {
+        if(!isRotation1 && !isRotation2 && !isRotation3) {
+            return arr.arr[point1.x][point1.y - 1] == 1 || arr.arr[point2.x][point2.y - 1] == 1 ||
+                    arr.arr[point4.x][point4.y - 1] == 1
+        } else if(isRotation1 && !isRotation2 && !isRotation3){
+            return arr.arr[point3.x][point3.y - 1] == 1 || arr.arr[point4.x][point4.y - 1] == 1
+        } else if(isRotation1 && isRotation2 && !isRotation3) {
+            return arr.arr[point1.x][point1.y - 1] == 1 || arr.arr[point2.x][point2.y - 1] == 1 ||
+                    arr.arr[point3.x][point3.y - 1] == 1
+        } else {
+            return arr.arr[point2.x][point2.y - 1] == 1 || arr.arr[point4.x][point4.y - 1] == 1
+        }
+    }
+
+    fun touchRightBlock(arr: CompareArray): Boolean {
+        if(!isRotation1 && !isRotation2 && !isRotation3) {
+            return arr.arr[point1.x][point1.y + 1] == 1 || arr.arr[point2.x][point2.y + 1] == 1 ||
+                    arr.arr[point3.x][point3.y + 1] == 1
+        } else if(isRotation1 && !isRotation2 && !isRotation3){
+            return arr.arr[point2.x][point2.y + 1] == 1 || arr.arr[point4.x][point4.y + 1] == 1
+        } else if(isRotation1 && isRotation2 && !isRotation3) {
+            return arr.arr[point1.x][point1.y + 1] == 1 || arr.arr[point2.x][point2.y + 1] == 1 ||
+                    arr.arr[point4.x][point4.y + 1] == 1
+        } else {
+            return arr.arr[point3.x][point3.y + 1] == 1 || arr.arr[point4.x][point4.y + 1] == 1
+        }
+    }
+
 
 }
